e9f7cb6c3a3103b95fc5369c641e890780c6d0a0
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-05-23 00:24:07 -0700

    Update next.config.js.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = (phase, { defaultConfig }) => {
   if (phase !== PHASE_DEVELOPMENT_SERVER) {
     return {
         trailingSlash: true,
-        assetPrefix: "."    
     }
   }
   return {
