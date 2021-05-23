ec7981e58bc12676206d3d3a9cfaf7dfc3724a69
Kento Kanazawa <kentokanazawa@yahoo.com>
2021-05-22 23:49:26 -0700

    Add Closing section component.

---
+++ components/Closing.tsx
@@ -0,0 +1,29 @@
+import { BaseSection } from "./BaseSection";
+import { Container } from "reactstrap";
+import React from "react";
+import { Heading } from "./Heading";
+
+export const Closing = () => {
+  return (
+    <BaseSection sectionClassName="closing d-flex align-items-center">
+      <Container>
+        <div className="d-flex flex-column justify-content-center text-center align-items-center">
+          <Heading
+            headingLabel="I'm Available For Freelance Projects"
+            headingType="primary"
+            color="primary"
+            size="lg"
+            className="mb-5"
+          />
+          <p className="closing__remarks">
+            Need a landing page? An e-commerce site? I'm open to working on any
+            intersting projects. Feel free to contact me at{" "}
+            <a href="mailto:kento@kentokanazawa.com">
+              kento[at]kentokanazawa.com
+            </a>
+          </p>
+        </div>
+      </Container>
+    </BaseSection>
+  );
+};
